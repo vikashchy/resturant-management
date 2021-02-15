0c5058de598dfe3546fac6f36c44929f658f074c
Vikash Choudhary <er.vikashchy@gmail.com>
2021-02-15 16:44:34 +0530

    cleanup

--- app_config.py
+++ app_config.py
@@ -2,8 +2,3 @@ import os
 
 DB_HOST_URL = os.environ.get('DB_HOST_URL', "sqlite:///data.db")
 
-#   Smtp config
-MAIL_PORT = 587
-MAIL_PASSWORD = "Zoom_123"
-SMTP_SERVER = "smtp.gmail.com"
-MAIL_USER = "er.vikashchy@gmail.com"
